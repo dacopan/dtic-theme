b2c92907bab116116a7b5d9afd3e2187de3c3c99
anunaki <anunaki@uENLIL>
2015-09-24 13:50:09 -0500

    add gulp task

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,15 +1,56 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')({lazy: false});
+var rimraf = require('rimraf');
 
-gulp.task('aaaa', function () {
-    // place code for your default task here
+
+
+var paths = {
+    less: ['./resources/assets/less/dtictheme.less'],
+    js: ['./resources/assets/js/*.js', './resources/assets/js/**/*.js'],
+    dist: {
+        css: './resources/primefaces-dtic_theme/',
+        js: './resources/primefaces-dtic_theme/js'
+    },
+    test: ['./js/tests/**.js']
+};
+
+/**
+ * removes css- and js-dist folder.
+ */
+gulp.task('clean', function () {
+    rimraf.sync(paths.dist.css + 'theme.css');
+    rimraf.sync(paths.dist.js);
 });
 
-gulp.task('default', function () {
-    // place code for your default task here
+/**
+ * compiles less files into css.
+ */
+gulp.task('less', function () {
+    gulp.src(paths.less)
+            .pipe(plugins.concat('theme.css'))
+            .pipe(plugins.less())
+            .pipe(plugins.minifyCss())
+            .pipe(gulp.dest(paths.dist.css));
 });
+
+
+gulp.task('js', function () {
+    gulp.src(paths.js)
+            .pipe(plugins.concat('dt1ctheme.js'))
+            .pipe(plugins.uglify({mangle: false}))
+            .pipe(gulp.dest(paths.dist.js));
+});
+
+
+/**
+ * optimizes the output in terms of minification and concatenation.
+ */
+gulp.task('dist', function () {
+    // add some optimizations (?)
+});
+
+
+
+gulp.task('build', ['clean', 'less', 'js', 'dist']);
+
+gulp.task('default', ['build']);
