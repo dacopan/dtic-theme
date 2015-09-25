dfd47d125322b35403fdd1fd9d253f845def0b75
anunaki <anunaki@uENLIL>
2015-09-25 13:32:48 -0500

    add wizard

--- src/java/io/dacopancm/dt1ctheme/showcase/view/CarService.java
+++ src/java/io/dacopancm/dt1ctheme/showcase/view/CarService.java
@@ -3,9 +3,9 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package io.dacopancm.dt1ctheme;
+package io.dacopancm.dt1ctheme.showcase.view;
 
-import io.dacopancm.dt1ctheme.model.Car;
+import io.dacopancm.dt1ctheme.showcase.model.Car;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
